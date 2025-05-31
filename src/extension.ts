25d23442ace2e1448cb58170ca4dfa4e46d6b7f8
SakuraMYK <1003737847@qq.com>
2025-05-31 13:25:42 +0800

    build: 更新扩展图标并移除 HelloWorld 命令
    - 在 package.json 中添加扩展图标路径
    - 从 package.json 中移除 HelloWorld 命令的贡献
    - 从 extension.ts 中移除 HelloWorld 命令的注册

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,6 @@ export function activate(context: vscode.ExtensionContext) {
   console.log("========================================");
   console.error("====== PyCodeJOJO extension activated  ======");
   console.warn("====== warning  ======");
-  const disposable = vscode.commands.registerCommand(
-    "pycodejojo.helloWorld",
-    () => {}
-  );
-
-  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
