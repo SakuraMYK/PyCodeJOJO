7a7ae10f055866947657298e637df277db12e823
SakuraMYK <1003737847@qq.com>
2025-06-03 15:57:47 +0800

    feat(extension): 添加颜色选择器功能
    - 在 extension.ts 中添加 ColorPicker 类的引用和注册
    - 新增 colorPicker.ts 文件实现颜色选择器功能
    - 支持 RGB、RGBA 和十六进制颜色值的识别和显示

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,15 @@
 import * as vscode from "vscode";
+import { ColorPicker } from "./features/colorPicker";
+
+let enable_ColorPicker = true;
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log("========================================");
-  console.error("====== PyCodeJOJO extension activated  ======");
-  console.warn("====== warning  ======");
+  if (enable_ColorPicker) {
+    context.subscriptions.push(
+      vscode.languages.registerColorProvider("*", new ColorPicker())
+    );
+  }
 }
 
 export function deactivate() {}
+  
---
+++ src/features/colorPicker.ts
@@ -0,0 +1,167 @@
+import vscode from "vscode";
+
+const reRGB = /rgb\((\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)/gs;
+
+const reRGBA =
+  /rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3}|1|0|0\.\d+)\s*\)/gs;
+
+const reTupleRGB =
+  /(?<!rgb)(?<!rgba)\((\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)/gs;
+
+const reTupleRGBA =
+  /(?<!rgb)(?<!rgba)\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3}|1|0|0\.\d+)\s*\)/gs;
+
+const reHex = /#([0-9A-Fa-f]{8}|[0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})\b/gs;
+
+interface ColorMap {
+  range: vscode.Range;
+  text: string;
+  color: vscode.Color;
+}
+
+export class ColorPicker implements vscode.DocumentColorProvider {
+  constructor() {}
+
+  provideDocumentColors(
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken
+  ): vscode.ColorInformation[] {
+    const colors: vscode.ColorInformation[] = [];
+    const maps = [
+      ...getTupleRGBMaps(document),
+      ...getTupleRGBAMaps(document),
+      ...getHexMaps(document),
+    ];
+    for (const map of maps) {
+      colors.push(new vscode.ColorInformation(map.range, map.color));
+    }
+    return colors;
+  }
+
+  provideColorPresentations(
+    color: vscode.Color,
+    context: { document: vscode.TextDocument; range: vscode.Range },
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.ColorPresentation[]> {
+    return [];
+  }
+}
+
+function getTupleRGBMaps(document: vscode.TextDocument): ColorMap[] {
+  const maps: ColorMap[] = [];
+  const matches = [...document.getText().matchAll(reTupleRGB)];
+  for (const match of matches) {
+    const s = match.index;
+    const e = match.index + match[0].length;
+    const start = document.positionAt(s);
+    const end = document.positionAt(e);
+    const R = parseInt(match[1]);
+    const G = parseInt(match[2]);
+    const B = parseInt(match[3]);
+    const text = `rgba(${R}, ${G}, ${B}, 1)`;
+
+    if (R >= 0 && R <= 255 && G >= 0 && G <= 255 && B >= 0 && B <= 255) {
+      maps.push({
+        range: new vscode.Range(start, end),
+        text: text,
+        color: new vscode.Color(R / 255, G / 255, B / 255, 1),
+      });
+    }
+  }
+  return maps;
+}
+
+function getTupleRGBAMaps(document: vscode.TextDocument): ColorMap[] {
+  const maps: ColorMap[] = [];
+  const matches = [...document.getText().matchAll(reTupleRGBA)];
+  for (const match of matches) {
+    const s = match.index;
+    const e = match.index + match[0].length;
+    const start = document.positionAt(s);
+    const end = document.positionAt(e);
+    const R = parseInt(match[1]);
+    const G = parseInt(match[2]);
+    const B = parseInt(match[3]);
+    let A = parseFloat(match[4]);
+    const text = `rgba(${R}, ${G}, ${B}, ${A})`;
+
+    if (
+      R >= 0 &&
+      R <= 255 &&
+      G >= 0 &&
+      G <= 255 &&
+      B >= 0 &&
+      B <= 255 &&
+      A >= 0 &&
+      A <= 255
+    ) {
+      A = A > 1 ? A / 255 : A;
+      maps.push({
+        range: new vscode.Range(start, end),
+        text: text,
+        color: new vscode.Color(R / 255, G / 255, B / 255, A),
+      });
+    }
+  }
+  return maps;
+}
+
+function getHexMaps(document: vscode.TextDocument): ColorMap[] {
+  function mapsPush(
+    start: vscode.Position,
+    end: vscode.Position,
+    text: string,
+    R: number,
+    G: number,
+    B: number,
+    A: number
+  ): void {
+    maps.push({
+      range: new vscode.Range(start, end),
+      text: text,
+      color: new vscode.Color(R / 255, G / 255, B / 255, A),
+    });
+  }
+
+  const maps: ColorMap[] = [];
+  const matches = [...document.getText().matchAll(reHex)];
+  for (const match of matches) {
+    const s = match.index;
+    const e = match.index + match[0].length;
+    const start = document.positionAt(s);
+    const end = document.positionAt(e);
+    const hex = match[1];
+    const hexLength = hex.length;
+    const text = match[0].toLowerCase();
+
+    let R;
+    let G;
+    let B;
+    let A;
+
+    switch (hexLength) {
+      case 3:
+        R = parseInt(hex[0] + hex[0], 16);
+        G = parseInt(hex[1] + hex[1], 16);
+        B = parseInt(hex[2] + hex[2], 16);
+        A = 1;
+        mapsPush(start, end, text, R, G, B, A);
+        break;
+      case 6:
+        R = parseInt(hex.substring(0, 2), 16);
+        G = parseInt(hex.substring(2, 4), 16);
+        B = parseInt(hex.substring(4, 6), 16);
+        A = 1;
+        mapsPush(start, end, text, R, G, B, A);
+        break;
+      case 8:
+        R = parseInt(hex.substring(0, 2), 16);
+        G = parseInt(hex.substring(2, 4), 16);
+        B = parseInt(hex.substring(4, 6), 16);
+        A = parseInt(hex.substring(6, 8), 16) / 255;
+        mapsPush(start, end, text, R, G, B, A);
+        break;
+    }
+  }
+  return maps;
+}
